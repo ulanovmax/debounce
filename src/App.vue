cf9a2fb3d1b072c0bd5cfd9732a43300458ef2c5
ulanovmax <ulanovmaks29@gmail.com>
2023-08-19 22:35:46 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template lang="pug">
+h1 Тестовое задание
+DebounceInput
+</template>
+
+<script setup>
+import DebounceInput from './components/DebounceInput/DebounceInput.vue'
+</script>
---
+++ src/components/DebounceInput/DebounceInput.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+input(placeholder="Введите что нибудь..", v-model.trim="inputValue")
+p Текущее значение:
+  span {{ (inputValue === '' ? 'Не указано' : inputValue) }}
+</template>
+
+
+<script setup>
+import {ref} from "vue";
+
+const inputValue = ref()
+</script>
+
+<style lang="scss">
+@import './DebounceInput.scss';
+</style>
